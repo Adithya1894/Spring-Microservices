42898990938779e307efa172d3e958dedad5218a
adithya1894 <adithya1894@gmail.com>
2018-03-18 18:21:04 -0400

    Added the Model pojos

---
+++ src/main/java/com/adithya/stock/dbservice/model/Quote.java
@@ -0,0 +1,46 @@
+package com.adithya.stock.dbservice.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "quotes")
+public class Quote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_name")
+    private String userName;
+
+    @Column(name = "quote")
+    private String quote;
+
+
+    public Quote() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getQuote() {
+        return quote;
+    }
+
+    public void setQuote(String quote) {
+        this.quote = quote;
+    }
+}
---
+++ src/main/java/com/adithya/stock/dbservice/model/Quotes.java
@@ -0,0 +1,4 @@
+package com.adithya.stock.dbservice.model;
+
+public class Quotes {
+}
